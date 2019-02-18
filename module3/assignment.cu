c2203a8b696ac308c7d94aa65db0e4b1dde15464
Zetser <mikhailwolff@gmail.com>
2019-02-18 19:33:35 +0000

    Counting distance from center pixel for every pixel in square array, and made so makefile builds the assignment.cu file.

--- module3/assignment.cu
+++ module3/assignment.cu
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include <chrono>
 
-
-__global__ void calcPixelPosition(int * d)
+__global__ void calcRelativePosition(int arraySize, int *rows, int *cols)
 {
-	uint i = (blockIdx.x * blockDim.x) + threadIdx.x;
-	//uint j = (blockIdx.y * blockDim.y) + threadIdx.y;
+	uint col = (blockIdx.x * blockDim.x) + threadIdx.x;
+	uint row = (blockIdx.y * blockDim.y) + threadIdx.y;
 	
-	int blockId = blockIdx.x+ blockIdx.y * gridDim.x;
-	int threadId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x)+ threadIdx.x;
+	//int blockId = blockIdx.x+ blockIdx.y * gridDim.x;
+	//int threadId = blockId * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x)+ threadIdx.x;
 	
-	d[threadId] = i;
+	int pixelPos = (row*arraySize)+col;
+	
+	rows[pixelPos] = row-arraySize/2;
+	cols[pixelPos] = col-arraySize/2;
 	//return threadId;
 	
-	printf("xPos: %3u yPos: %3u ", i, j);
+	//printf("xPos: %3u yPos: %3u \n", i, j);
 }
-
 
 int main(int argc, char** argv)
 {
@@ -44,20 +45,63 @@ int main(int argc, char** argv)
 		printf("The total number of threads will be rounded up to %d\n", totalThreads);
 	}
 	
-	int cpu_d[totalThreads] = {0};
-	int *dev_d;
-	cudaMalloc((void**)&dev_d, totalThreads*sizeof(int));
-	cudaMemset((void**)&dev_d, 0, totalThreads*sizeof(int));
+	// =======================================================================================
+	// Start my code
+	int totalThreadsSq = totalThreads*totalThreads;
+	printf("numBlocks: %d blockSize: %d\n", numBlocks, blockSize);
+	const dim3 blocks(numBlocks, numBlocks);
+	const dim3 threads(blockSize, blockSize);
+	
+	int cpu_r[totalThreadsSq] = {0};
+	int cpu_c[totalThreadsSq] = {0};
+	int *dev_r, *dev_c;
+	
+	cudaMalloc((void**)&dev_r, totalThreadsSq*sizeof(int));
+	cudaMalloc((void**)&dev_c, totalThreadsSq*sizeof(int));
+	cudaMemset((void**)&dev_r, 0, totalThreadsSq*sizeof(int));
+	cudaMemset((void**)&dev_c, 0, totalThreadsSq*sizeof(int));
 	
 	
-	auto start = std::chrono::high_resolution_clock::now();
+	auto start_gpu = std::chrono::high_resolution_clock::now();
+
+	calcRelativePosition<<<blocks,threads>>>(totalThreads, dev_r, dev_c);
 	
-	calcPixelPosition<<<numBlocks,blockSize>>>(dev_d);
+	auto stop_gpu  = std::chrono::high_resolution_clock::now();
 	
-	auto stop = std::chrono::high_resolution_clock::now();
+	cudaMemcpy(cpu_r, dev_r, totalThreadsSq*sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpu_c, dev_c, totalThreadsSq*sizeof(int), cudaMemcpyDeviceToHost);
+	cudaFree(dev_r);
+	cudaFree(dev_c);
 	
-	cudaMemcpy(cpu_d, dev_d, totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
-	cudaFree(dev_d);
+	/*for(int i=0; i<totalThreadsSq; i++) {
+		printf("Position from center: (%d,%d)\n", cpu_r[i], cpu_c[i]);
+	}*/
+	std::chrono::duration<double> diff_gpu = stop_gpu-start_gpu;
+	printf("Time to find relative positions of %dx%d array using GPU: %f s\n",
+				 totalThreads, totalThreads, diff_gpu.count());
+				 
+	cudaDeviceSynchronize();
+	// ===========================================================================
+	// Now repeat using CPU only		 
+	int r[totalThreadsSq] = {0};
+	int c[totalThreadsSq] = {0};
 	
-    cudaDeviceSynchronize();
+	auto start_cpu = std::chrono::high_resolution_clock::now();
+	
+	for(int i=0; i<totalThreads; i++) {
+		for(int j=0; j<totalThreadsSq; j++) {
+			r[i*totalThreads+j] = i-totalThreads/2;
+			c[i*totalThreads+j] = j-totalThreads/2;
+		}
+	}
+	auto stop_cpu  = std::chrono::high_resolution_clock::now();
+	
+	/*for(int i=0; i<totalThreadsSq; i++) {
+		printf("Position from center: (%d,%d)\n", r[i], c[i]);
+	}*/
+	std::chrono::duration<double> diff_cpu = stop_cpu-start_cpu;
+	printf("Time to find relative positions of %dx%d array using CPU: %f s\n",
+				 totalThreads, totalThreads, diff_cpu.count());
+	
+    
 }
