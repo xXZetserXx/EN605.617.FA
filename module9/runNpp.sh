3204c64aa8cf8c516644c4839d975fa0474b9277
Zetser <mikhailwolff@gmail.com>
2019-04-08 19:58:50 -0400

    Added runscripts.

---
+++ module9/runNpp.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+make target1
+./nppExec input=../common/data/Lena.bmp
+./nppExec input=../common/data/flower_gold.png
---
+++ module9/runThrustSort.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+make target2
+./thrustExec 
+./thrustExec 1048576
+./thrustExec 2073600
